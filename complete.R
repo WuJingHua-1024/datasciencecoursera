6a1417bf7d76572a25a91644fc5c947c3d51afa9
WuJingHua-1024 <68210986+WuJingHua-1024@users.noreply.github.com>
2020-07-30 11:11:03 +0800

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,13 +1,12 @@
 complete<-function(directory,id=1:332){
   directory<-list.files("/Users/willsong/Desktop/zaizai/R programming in data science/specdata",pattern = NULL)
   b<-c()
-  m=1
   for (i in id) {
     
     x<-read.csv(directory[i])
     y<-as.integer(!is.na(x[,2]))
     z<-as.integer(!is.na(x[,3]))
-    m=m+1
+    
     if(sum(y)<=sum(z)){
       a<-cbind(i,sum(y))
       b<-rbind(b,a)
